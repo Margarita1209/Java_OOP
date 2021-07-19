0c3a6c3a31e135b2a5390883832f376b52d22587
Margarita1209 <82923124+Margarita1209@users.noreply.github.com>
2021-07-19 13:01:58 -0400

    Add files via upload

---
+++ MaestroObjeto/src/com/maestroobjeto/HumanTest.java
@@ -0,0 +1,12 @@
+package com.maestroobjeto;
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		Human per = new Human();
+		Object Human = null;
+		per.attack(Human);
+		
+	}
+
+}
